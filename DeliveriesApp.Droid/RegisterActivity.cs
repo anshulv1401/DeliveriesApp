2f063a31c0f82dc3c2d9913144418a9e6d574e1a
anshulv1401 <31562315+anshulv1401@users.noreply.github.com>
2018-11-23 21:53:05 +0530

    Initial commit

---
+++ DeliveriesApp.Droid/RegisterActivity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DeliveriesApp.Droid
+{
+    [Activity(Label = "RegisterActivity")]
+    public class RegisterActivity : Activity
+    {
+        EditText emailEditText, passwordEditText, confirmPasswordEditText;
+        Button registerButton;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_register);
+
+            emailEditText = FindViewById<EditText>(Resource.Id.registerEmailEditText);
+            passwordEditText = FindViewById<EditText>(Resource.Id.registerPasswordEditText);
+            confirmPasswordEditText = FindViewById<EditText>(Resource.Id.confirmPasswordEditText);
+            registerButton = FindViewById<Button>(Resource.Id.registerUserButton);
+
+            registerButton.Click += RegisterButton_Click;
+
+            string email = Intent.GetStringExtra("Email");
+            emailEditText.Text = email;
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(passwordEditText.Text))
+            {
+                if (passwordEditText.Text == confirmPasswordEditText.Text)
+                {
+                    var user = new User()
+                    {
+                        Email = emailEditText.Text,
+                        Password = passwordEditText.Text
+                    };
+                    LoginActivity.MobileService.GetTable<User>
+                }
+                Toast.MakeText(this, "Password don't match", ToastLength.Short).Show();
+            }
+            Toast.MakeText(this, "Password can not be empty", ToastLength.Short).Show();
+        }
+    }
+}
